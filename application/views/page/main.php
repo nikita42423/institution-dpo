454afb7add770f7c1ace7460b9dcf071fe022e8e
nikita42423 <nikita42423@bk.ru>
2023-05-13 10:30:42 +0300

    a

---
+++ application/views/page/main.php
@@ -0,0 +1,323 @@
+<section id="glav">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-10" style="margin-top: 2%;">
+
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                          <div class="carousel-item active"  data-bs-interval="5000">
+                            <img src="img/1.jpg" class="d-block w-100" alt="..." height="500">
+                          </div>
+                          <div class="carousel-item"  data-bs-interval="5000">
+                            <img src="img/2.jpg" class="d-block w-100" alt="..." height="500">
+                          </div>
+                          <div class="carousel-item"  data-bs-interval="5000">
+                            <img src="img/3.jpg" class="d-block w-100" alt="..." height="500">
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+        </div>
+    </section>
+
+    <section id="curs">
+        <div class="container">
+            <div class="row">
+                <h1>КУРСЫ</h1>
+                <div class="col-lg-3">
+                    <h3>Фильтры для курсов</h3><hr>
+                    <form class="row g-3 needs-validation" novalidate>
+                        <div class="col-12">
+                            <label for="validationCustom04" class="form-label">Направление подготовки</label>
+                            <select class="form-select" id="validationCustom04">
+                              <option value="">...</option>
+                            </select>
+                          </div>
+                          <div class="col-12">
+                            <label for="validationCustom04" class="form-label">Уровень образование</label>
+                            <select class="form-select" id="validationCustom04">
+                              <option value="">...</option>
+                            </select>
+                          </div>
+                          <div class="col-12">
+                            <label for="validationCustom04" class="form-label">Форма обучения</label>
+                            <select class="form-select" id="validationCustom04">
+                              <option value="">...</option>
+                            </select>
+                          </div>
+                          <div class="col-12">
+                            <label for="validationCustom04" class="form-label">Срок обучения</label>
+                            <select class="form-select" id="validationCustom04">
+                              <option value="">...</option>
+                            </select>
+                          </div>
+                        <div class="col-12 fnt">
+                          <button class="btn btn-primary" type="submit">поиск</button>
+                        </div>
+                      </form>
+                      <hr>
+                </div>
+
+
+
+                <div class="col-lg-9">
+
+                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="col">
+                          <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title ">Название</h5>
+                              <p class="card-text ty"><b>Цена:</b> 15000 рублей</p>
+                              <div class="ty">
+                              <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+    Подробнее
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+         
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+       w
+        </div>
+       
+      </div>
+    </div>
+  </div>
+
+  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+    Заявка
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+   
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+    f
+        </div>
+        <div class="modal-footer">
+
+        </div>
+      </div>
+    </div>
+  </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                              <div class="card-body">
+                                <h5 class="card-title tt">Название</h5>
+                                <p class="card-text ty"><b>Цена:</b> 15000 рублей</p>
+                                <div class="ty">
+                                <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+      Подробнее
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+           
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+         w
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  
+    <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+      Заявка
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+     
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+      f
+          </div>
+          <div class="modal-footer">
+  
+          </div>
+        </div>
+      </div>
+    </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <div class="card">
+                              <div class="card-body">
+                                <h5 class="card-title tt">Название</h5>
+                                <p class="card-text ty"><b>Цена:</b> 15000 рублей</p>
+                                <div class="ty">
+                                <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+      Подробнее
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+           
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+         w
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  
+    <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+      Заявка
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+     
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+      f
+          </div>
+          <div class="modal-footer">
+  
+          </div>
+        </div>
+      </div>
+    </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <div class="card">
+                              <div class="card-body">
+                                <h5 class="card-title tt">Название</h5>
+                                <p class="card-text ty"><b>Цена:</b> 15000 рублей</p>
+                                <div class="ty">
+                                <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+      Подробнее
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+           
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+         w
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  
+    <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+      Заявка
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+     
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+      		модальное окно
+          </div>
+          <div class="modal-footer">
+  
+          </div>
+        </div>
+      </div>
+    </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="aboutus" style="margin-bottom: 3%;">
+		<div class="container">
+			<div class="clearfix">
+				<img src="img/4.jpg" class="col-md-6 float-md-start mb-3 ms-md-3 p-3" alt="...">
+				<div class="dd">
+					<p><b>Цель проекта Все Курсы Онлайн - помогать людям познавать новую информацию каждый день и добиваться успеха в жизни.</b></p> 
+					<p>Для этого был создан удобный сайт со всеми образовательными программами онлайн и дистанционными курсами со всего мира.</p>
+					<p>Главные достоинства проекта - удобный поиск курсов при помощи фильтров и категорий. Можно отобрать только платные курсы онлайн или только курсы онлайн для детей и взрослых. Отдельными спецпроектами выделены образовательные видео и вебинары. Также крайне полезна возможность записаться на курс или уточнить дополнительную информацию посредством функционала сайта Все Курсы Онлайн.</p> 
+					<p>Все события рынка онлайн-образования освещаются на Всех Курсах Онлайн.</p>
+					<p>На сайте - удобный дизайн, адаптированный к любым размерам экрана, в том числе будет удобен для просмотра с экранов мобильных устройств.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<p class="dd">Размещение базовой информации о требующих оплаты курсах, событиях, вебинарах для онлайн центров обучения - платное.</p>
+			</div>
+		</div>
+		
+
+    </section>
+
