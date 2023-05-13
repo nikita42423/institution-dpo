4e0ebba1a1e5108c7deed4ee40d2a2e58424817a
ilyakharlamov2023 <ilyakh_usa@mail.ru>
2023-05-13 09:58:51 +0300

    d

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,2 +1,45 @@
-    </body>
+<footer id="foot">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="ft1">
+                        <img src="<?=asset_url()?>/img/log.png" alt="" width="150" height="120">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ft2">
+                        <nav class="nav flex-column">
+                            <a class="nav-link active" aria-current="page" href="#glav">ГЛАВНАЯ</a>
+                            <a class="nav-link" href="#curs">КУРСЫ</a>
+                            <a class="nav-link" href="#aboutus">О НАС</a>
+                          </nav>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ft3">
+                        <h4>СОЦИАЛЬНЫЕ СЕТИ</h4>
+                        <span>
+                            <a href="https://education.vk.company/pages/index/"><img src="<?=asset_url()?>/img/vk.png" alt="" width="25" height="25" ></a>
+                            <a href="https://t.me/garantobrazovanie"><img src="<?=asset_url()?>/img/teleg.png" alt="" width="25" height="25" ></a>
+                            <a href="tel:+79004350002"><img src="<?=asset_url()?>/img/whats.png" alt="" width="25" height="25" ></a>
+                        </span><hr>
+                        <h4>КОНТАКТНЫЕ</h4>
+                        <span>+7(900) 435 - 00 - 02</span>&nbsp;&nbsp;&nbsp;
+                        <span>miadeaf@mail.ru</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <footer id="foot2">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 fft">
+            <p>&copy; 2023г Учебный центр "МИА-DEAF" | Все права защищены</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+</body>
 </html>
