6d46e7a51bb5c40bda35302d07560c22b9129ca9
ilyakharlamov2023 <ilyakh_usa@mail.ru>
2023-05-13 14:15:58 +0300

    rt

--- application/views/page/main.php
+++ application/views/page/main.php
@@ -116,14 +116,39 @@
   <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-   
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
         <div class="modal-body">
-    f
-        </div>
-        <div class="modal-footer">
+   
+        <section class=" bg-image">
+  
+  <div class="col-12 ">
+    <div class="card" style="border-radius: 15px;">
+      <div class="card-body p-5">
+        <h2 class="text-uppercase text-center mb-5">ЗАЯВКА</h2>
+
+        <form>
+
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example1cg" class="form-control form-control-lg" required />
+            <label class="form-label" for="form3Example1cg">ФИО</label>
+          </div>
+
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example2cg" class="form-control form-control-lg" required/>
+            <label class="form-label" for="form3Example2cg">Телефон</label>
+          </div>
+          
+          <div class="d-flex justify-content-center">
+            <button type="submit"
+              class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">ЗАЯВКА</button>
+          </div>
+
+        </form>
+
+      </div>
+</div>
+</div>
+</section>
+
 
         </div>
       </div>
