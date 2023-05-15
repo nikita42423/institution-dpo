4904d42ba45482afc3e713588e3b9cb729a5e84d
ilyakharlamov2023 <ilyakh_usa@mail.ru>
2023-05-15 15:18:04 +0300

    г

--- application/controllers/Buxgalter.php
+++ application/controllers/Buxgalter.php
@@ -5,17 +5,17 @@ class Buxgalter extends CI_Controller {
 
 	public function index()
 	{
+         //Сессия
+         $data['session'] = $this->session->userdata('login_session');
+         $session=$data['session'];
+         $ID_user = $session['ID_user'];
+
 		$this->load->view('template/header.php');
-		$this->load->view('page/buxgalter.php');
+		$this->load->view('page/buxgalter.php',  $data);
 		$this->load->view('template/footer.php');
 	}
 
-    public function index2()
-	{
-		$this->load->view('template/header.php');
-		$this->load->view('page/buxgalter2.php');
-		$this->load->view('template/footer.php');
-	}
+   
 
 	
 }
