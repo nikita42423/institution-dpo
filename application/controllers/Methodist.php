4514a331538c2741c87d08d9b23afbd45e4f69c9
nikita42423 <nikita42423@bk.ru>
2023-05-15 08:34:13 +0300

    ф

--- application/controllers/Methodist.php
+++ application/controllers/Methodist.php
@@ -6,8 +6,25 @@ class Methodist extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('template/header.php');
-		$this->load->view('template/navbar_methodist.php');
 		$this->load->view('page/methodist.php');
 		$this->load->view('template/footer.php');
+	}
+
+	public function add_type_ep()
+	{
+		if (!empty($_POST))
+        {
+            $data = array(
+                'name_type_ep' => $this->input->post('name_type_ep')
+            );
+
+            $this->load->model('info_m');
+            $this->info_m->add_type_ep($data);
+
+            //Сообщение об успеха
+            $this->session->set_flashdata('msg', 'Успешно добавлен!');
+
+            redirect(base_url('methodist/index'));
+        }
 	}
 }
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <title>Учебный центр</title>
     <link href="<?=asset_url()?>/css/bootstrap.min.css" rel="stylesheet">
-    <script src="<?=asset_url()?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=asset_url()?>/js/bootstrap.bundle.min.js" rel="stylesheet"></script>
     <link href="<?=asset_url()?>/css/style.css" rel="stylesheet">
-    <script src="<?=asset_url()?>/js/jquery-3.7.0.min.js"></script>
+    
+    <script src="<?=asset_url()?>/jsTable/jquery-3.6.4.min.js"></script>
+    <script src="<?=asset_url()?>/jsTable/datatables.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+
 </head>
 <body>
