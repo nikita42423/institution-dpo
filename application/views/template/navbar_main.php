6b4076808f46330cc6f974c0c465a3f0e8179bd2
ilyakharlamov2023 <ilyakh_usa@mail.ru>
2023-05-17 10:35:38 +0300

    l

--- application/views/template/navbar_main.php
+++ application/views/template/navbar_main.php
@@ -1,10 +1,46 @@
 <header id="head">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3">
-                <img src="<?=asset_url()?>/img/log.png" alt="" width="120" height="100">
+            <div class="col-lg-12">
+            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
+            </button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Фон с прокруткой</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Закрыть"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Попробуйте прокрутить оставшуюся часть страницы, чтобы увидеть эту опцию в действии.</p>
+  </div>
+</div>
+               
             </div>
-            <div class="col-lg-6">
+           
+
+           
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+
+
+ <img src="<?=asset_url()?>/img/log.png" alt="" width="120" height="100">
+
+
+
+
+
+
+<div class="col-lg-6">
                 <div class="nav">
                     <nav class="navbar navbar-expand-lg navbar-light">
                         <div class="container-fluid">
@@ -37,6 +73,3 @@
                     </li>
                     </ul>
             </div>
-        </div>
-    </div>
-</header>
