53509eee053e13f311240f6c1af0899c38910ef3
ilyakharlamov2023 <ilyakh_usa@mail.ru>
2023-05-18 16:48:58 +0300

    d

---
+++ application/views/page/clients.php
@@ -0,0 +1,93 @@
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Личный кабинет</h1>
+
+            <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+       Персональные данные
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+       
+      <form action="" method="post">
+
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example1cg" class="form-control form-control-lg" required name="full_name"/>
+            <label class="form-label" for="form3Example1cg">ФИО</label>
+          </div>
+
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example2cg" class="form-control form-control-lg" required  name="phone"/>
+            <label class="form-label" for="form3Example2cg">Телефон</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example3cg" class="form-control form-control-lg" required   name="adres"/>
+            <label class="form-label" for="form3Example3cg">Адрес</label>
+          </div>
+          <div class="d-flex justify-content-center">
+            <button type="submit"
+              class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">сохранить</button>
+          </div>
+
+        </form>
+
+
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Мои курсы
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+
+
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Курсы</th>
+      <th scope="col">Направление</th>
+      <th scope="col">Наименование ОП</th>
+      <th scope="col">Вид</th>
+      <th scope="col">Дата начала обучения</th>
+      <th scope="col">Дата окончания обучения</th>
+      <th scope="col">Телефон</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Статус</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>fdfdd</td>
+      <td>fdfd</td>
+      <td>fdf</td>
+      <td>@fdfd</td>
+      <td>@fdfd</td>
+      <td>fdfd</td>
+      <td>fdf</td>
+      <td>@fdfd</td>
+      <td>@fdfd</td>
+    </tr>
+
+  </tbody>
+
+
+
+
+    </div>
+    </div>
+  </div>
+</div>
+
+            </div>
+        </div>
+    </div>
+</section>
